9529a18ea1ad2bb8320b23ce9f71f056dd1efccc
Jeremy Faria <fari9860@mylaurier.ca>
2024-06-08 13:05:50 -0400

    roughwork completed

---
+++ roughwork.R
@@ -0,0 +1,10 @@
+library(survivoR)
+library(dplyr)
+library(stringr)
+library(ggplot2)
+castaway_details <- castaway_details
+us_castaways <- dplyr::filter(castaway_details, grepl("^US", castaway_details$castaway_id))
+castaways_id_season <- dplyr::select(castaways, castaway_id, season)
+us_castaways_full <- left_join(us_castaways, castaways_id_season, by = "castaway_id")
+bipoc_per_season <- us_castaways_full  %>% group_by(season) %>% summarise(bipoc_percentage = (sum(bipoc)/length(unique(castaway_id)) * 100))
+ggplot() + geom_bar(aes(x=bipoc_per_season$season,y=bipoc_per_season$bipoc_percentage), stat="identity", fill = "")
