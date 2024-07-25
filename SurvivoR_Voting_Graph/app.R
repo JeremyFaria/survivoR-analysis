da6efb598734c9852863decc290a1ee310381dcb
Jeremy Faria <fari9860@mylaurier.ca>
2024-07-25 19:27:47 -0400

    filtering issue complete

--- SurvivoR_Voting_Graph/app.R
+++ SurvivoR_Voting_Graph/app.R
@@ -14,24 +14,9 @@ library(dplyr)
 
 boot_mapping <- boot_mapping
 vote_history <- vote_history
-generate_voting_graph <- function(season, tribe){
-  season <- "Survivor: Borneo"
-  tribe <- "Tagi"
-  tribe_status <- "Original"
-  boot_mapping_original <- dplyr::filter(boot_mapping)
-  vote_history_boot_map_og <- inner_join(vote_history, boot_mapping_original, by = c('season' = 'season', 'episode' = 'episode', 'castaway' = 'castaway'))
-  vote_history_boot_map_og_tribe <- dplyr::filter(vote_history_boot_map_og, season_name.x %in% season, tribe.x %in% tribe, tribe_status.x %in% tribe_status)
-  season_before_graph <- vote_history_boot_map_og_tribe %>% select(castaway, vote)
-  season_before_graph <- season_before_graph[complete.cases(season_before_graph), ]
-  voters_s = season_before_graph[['castaway']]
-  voted_s = season_before_graph[['vote']]
-  season_ready_for_graph = c(rbind(voters_s, voted_s))
-  return(graph(season_ready_for_graph))
-}
 
 Seasons <- as.vector(unique(vote_history$season_name))
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
   
   sidebarLayout(
@@ -43,17 +28,6 @@ ui <- fluidPage(
     )
   ))
 
-    # Application title
-    #titlePanel("Old Faithful Geyser Data"),
-    
-  #  selectInput("Season","Choose a season", Seasons),
-   # uiOutput("Tribe")
-    #)
-    #mainPanel(
-     # plotOutput("Graph")
-    #)
-
-# Define server logic required to draw a histogram
 server <- function(input, output) {
 
     output$Tribe <- renderUI({
@@ -67,7 +41,7 @@ server <- function(input, output) {
       tribe <- input$Tribe
       tribe_status <- "Original"
       boot_mapping_original <- dplyr::filter(boot_mapping)
-      vote_history_boot_map_og <- inner_join(vote_history, boot_mapping_original, by = c('season' = 'season', 'episode' = 'episode', 'castaway' = 'castaway'))
+      vote_history_boot_map_og <- inner_join(vote_history, boot_mapping_original, by = c('season' = 'season', 'episode' = 'episode', 'castaway' = 'castaway', 'version' = 'version'))
       vote_history_boot_map_og_tribe <- dplyr::filter(vote_history_boot_map_og, season_name.x == !!season, tribe.x == !!tribe, tribe_status.x == !!tribe_status)
       season_before_graph <- vote_history_boot_map_og_tribe %>% select(castaway, vote)
       season_before_graph <- season_before_graph[complete.cases(season_before_graph), ]
@@ -79,21 +53,6 @@ server <- function(input, output) {
     output$Graph <- renderPlot(
       plot(graph(data_for_graph()))
     )
-    
-    # output$Graph <- renderPlot({
-    #   season <- input$Season
-    #   tribe <- renderText(output$Tribe)
-    #   tribe_status <- "Original"
-    #   boot_mapping_original <- dplyr::filter(boot_mapping)
-    #   vote_history_boot_map_og <- inner_join(vote_history, boot_mapping_original, by = c('season' = 'season', 'episode' = 'episode', 'castaway' = 'castaway'))
-    #   vote_history_boot_map_og_tribe <- renderDataTable({dplyr::filter(vote_history_boot_map_og, season_name.x == !!season, tribe.x == !!tribe, tribe_status.x == !!tribe_status)})
-    #   season_before_graph <- vote_history_boot_map_og_tribe %>% renderDataTable({select(castaway, vote)})
-    #   season_before_graph <- renderDataTable({season_before_graph[complete.cases(season_before_graph), ]})
-    #   voters_s = renderDataTable({season_before_graph[['castaway']]})
-    #   voted_s = renderDataTable({season_before_graph[['vote']]})
-    #   season_ready_for_graph = renderDataTable({c(rbind(voters_s, voted_s))})
-    #   plot(graph(dataTableOutput("season_ready_for_graph")))
-    # })
 }
 
 # Run the application 
--- Voting_Graph_script.R
+++ Voting_Graph_script.R
@@ -5,11 +5,13 @@ library(igraph)
 
 boot_mapping <- boot_mapping
 vote_history <- vote_history
+#df[!duplicated(df), ]
 season <- "Survivor: Borneo"
 tribe <- "Tagi"
 tribe_status <- "Original"
-boot_mapping_original <- dplyr::filter(boot_mapping)
-vote_history_boot_map_og <- inner_join(vote_history, boot_mapping_original, by = c('season' = 'season', 'episode' = 'episode', 'castaway' = 'castaway'))
+boot_mapping_original <- dplyr::filter(boot_mapping, boot_mapping$game_status == "In the game")
+boot_mapping_original$tribe_unique <- paste(boot_mapping_original$tribe, "-", boot_mapping_original$tribe_status)
+vote_history_boot_map_og <- inner_join(vote_history, boot_mapping_original, by = c('season' = 'season', 'episode' = 'episode', 'castaway' = 'castaway', 'version' = 'version'))
 vote_history_boot_map_og_tribe <- dplyr::filter(vote_history_boot_map_og, season_name.x == !!season, tribe.x == !!tribe, tribe_status.x == !!tribe_status)
 season_before_graph <- vote_history_boot_map_og_tribe %>% select(castaway, vote)
 season_before_graph <- season_before_graph[complete.cases(season_before_graph), ]
@@ -17,6 +19,5 @@ voters_s = season_before_graph[['castaway']]
 voted_s = season_before_graph[['vote']]
 season_ready_for_graph = c(rbind(voters_s, voted_s))
 plot(graph(season_ready_for_graph))
-
 
 
