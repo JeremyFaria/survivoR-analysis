8699d88a2abdd1da00ea525e91d4abfa6563ca45
Jeremy Faria <fari9860@mylaurier.ca>
2024-07-26 19:17:20 -0400

    updated filtering issues and ui

--- SurvivoR_Voting_Graph/app.R
+++ SurvivoR_Voting_Graph/app.R
@@ -13,15 +13,16 @@ library(igraph)
 library(dplyr)
 
 boot_mapping <- boot_mapping
+boot_mapping$tribe_unique <- paste(boot_mapping$tribe, "-", boot_mapping$tribe_status)
 vote_history <- vote_history
 
-Seasons <- as.vector(unique(vote_history$season_name))
+Seasons <- as.vector(unique(boot_mapping$season_name))
 
 ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      selectInput("Season","Choose a season", Seasons),
+      selectInput("Season","Select a season", Seasons),
       uiOutput("Tribe")), 
     mainPanel(
       plotOutput("Graph")
@@ -31,18 +32,20 @@ ui <- fluidPage(
 server <- function(input, output) {
 
     output$Tribe <- renderUI({
-        temp_season <- vote_history[vote_history$season_name == input$Season,]
-        temp_tribe <- as.vector(unique(temp_season$tribe))
+        temp_season <- boot_mapping[boot_mapping$season_name == input$Season,]
+        temp_tribe <- as.vector(unique(temp_season$tribe_unique))
         selectInput("Tribe", "Select a tribe", temp_tribe)
     })
     
     data_for_graph <- reactive({
       season <- input$Season
       tribe <- input$Tribe
-      tribe_status <- "Original"
-      boot_mapping_original <- dplyr::filter(boot_mapping)
+      boot_mapping_original <- boot_mapping
       vote_history_boot_map_og <- inner_join(vote_history, boot_mapping_original, by = c('season' = 'season', 'episode' = 'episode', 'castaway' = 'castaway', 'version' = 'version'))
-      vote_history_boot_map_og_tribe <- dplyr::filter(vote_history_boot_map_og, season_name.x == !!season, tribe.x == !!tribe, tribe_status.x == !!tribe_status)
+      vote_history_boot_map_og <- vote_history_boot_map_og %>% select(version:season_name.y,tribe_unique)
+      vote_history_boot_map_og <- vote_history_boot_map_og[!duplicated(vote_history_boot_map_og), ]
+      vote_history_boot_map_og_tribe <- dplyr::filter(vote_history_boot_map_og, season_name.y == !!season, tribe_unique == !!tribe)
+      vote_history_boot_map_og_tribe <- vote_history_boot_map_og_tribe[!duplicated(vote_history_boot_map_og_tribe), ]
       season_before_graph <- vote_history_boot_map_og_tribe %>% select(castaway, vote)
       season_before_graph <- season_before_graph[complete.cases(season_before_graph), ]
       voters_s <- season_before_graph[['castaway']]
--- Voting_Graph_script.R
+++ Voting_Graph_script.R
@@ -6,13 +6,15 @@ library(igraph)
 boot_mapping <- boot_mapping
 vote_history <- vote_history
 #df[!duplicated(df), ]
-season <- "Survivor: Borneo"
-tribe <- "Tagi"
+season <- "Survivor: Fiji"
+tribe <- "Moto - Original"
 tribe_status <- "Original"
 boot_mapping_original <- dplyr::filter(boot_mapping, boot_mapping$game_status == "In the game")
 boot_mapping_original$tribe_unique <- paste(boot_mapping_original$tribe, "-", boot_mapping_original$tribe_status)
 vote_history_boot_map_og <- inner_join(vote_history, boot_mapping_original, by = c('season' = 'season', 'episode' = 'episode', 'castaway' = 'castaway', 'version' = 'version'))
-vote_history_boot_map_og_tribe <- dplyr::filter(vote_history_boot_map_og, season_name.x == !!season, tribe.x == !!tribe, tribe_status.x == !!tribe_status)
+vote_history_boot_map_og <- vote_history_boot_map_og %>% select(version:season_name.y,tribe_unique)
+vote_history_boot_map_og <- vote_history_boot_map_og[!duplicated(vote_history_boot_map_og), ]
+vote_history_boot_map_og_tribe <- dplyr::filter(vote_history_boot_map_og, season_name.x == !!season, tribe_unique == !!tribe)
 season_before_graph <- vote_history_boot_map_og_tribe %>% select(castaway, vote)
 season_before_graph <- season_before_graph[complete.cases(season_before_graph), ]
 voters_s = season_before_graph[['castaway']]
